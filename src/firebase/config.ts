fc5d02d188ac0b878595afb0539177e4dcf5229f
AffiliateSync <affiliatesync0@gmail.com>
2026-03-20 03:17:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9886993662-50a10",
+  "appId": "1:801120435426:web:deb0b147fc0d84318bb2bb",
+  "apiKey": "AIzaSyB7kisgDgvdkuxX3FaECBgiJ9PGjec6CAI",
+  "authDomain": "studio-9886993662-50a10.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "801120435426"
+};
