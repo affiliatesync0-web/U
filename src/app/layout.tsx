069db6f14d672ad58404550ceecdee06d36f6a0e
AffiliateSync <affiliatesync0@gmail.com>
2026-05-01 19:38:58 +0000

    ve a la carpeta src y abre la otra carpeta que se llama app y cambia el

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const viewport: Viewport = {
   viewportFit: 'cover',
 };
 
-// URL OFICIAL DEL FAVICON (LINK DE FACEBOOK)
+// URL OFICIAL DEL FAVICON (ENLACE DE FACEBOOK PROPORCIONADO POR EL USUARIO)
 const FAVICON_URL = "https://scontent.fmga4-1.fna.fbcdn.net/v/t39.30808-6/666660077_122243300624253134_2093271733727861427_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=1d70fc&_nc_ohc=F1dEO0QGZq0Q7kNvwGU2lud&_nc_oc=AdoONEUitz3ItQ57a9wwS9h2v_ITSAuom5IOgKRog8U2RLE9eUBLbWxXdPGLPw9wT5M&_nc_zt=23&_nc_ht=scontent.fmga4-1.fna&_nc_gid=d-ZAMTuwDdRhfKDyit3fag&_nc_ss=7b289&oh=00_Af5quuYFfzIp66oolHNWOA1d-3PaBFViD3nt2qiOa1Rfxg&oe=69FA0EA1";
 
 export const metadata: Metadata = {
@@ -26,6 +26,9 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: FAVICON_URL, rel: 'icon', type: 'image/jpeg' },
+    ],
+    shortcut: [
+      { url: FAVICON_URL },
     ],
     apple: [
       { url: FAVICON_URL },
@@ -42,7 +45,8 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
       <head>
-        <link rel="icon" href={FAVICON_URL} />
+        {/* INYECCIÓN MANUAL PARA SOBREESCRIBIR CUALQUIER ARCHIVO LOCAL */}
+        <link rel="icon" href={FAVICON_URL} sizes="any" />
         <link rel="shortcut icon" href={FAVICON_URL} />
         <link rel="apple-touch-icon" href={FAVICON_URL} />
         <meta name="theme-color" content="#131921" />
